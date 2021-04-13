9dccb6f7778193bfb7f16bde5a9fae5ae2365299
Alex <js.fullstack.development@gmail.com>
2021-04-13 14:55:30 +0300

    react-router-dom

--- src/Components/AuthorBooks.jsx
+++ src/Components/AuthorBooks.jsx
@@ -2,6 +2,17 @@ import { Component } from 'react';
 
 class AuthorBooks extends Component {
   state = {};
+
+  componentDidMount() {
+    console.log(Number(this.props.match.params.authorId));
+    console.log(this.props.authors);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log(Number(this.props.match.params.authorId));
+    console.log(this.props.authors);
+  }
+
   render() {
     return <h1>Компонент книги автора</h1>;
   }
